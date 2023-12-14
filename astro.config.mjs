a04460759c7adf28c35b9b4d6e004f62810e5fec
hiroshijp <hiroshijp0616@gmail.com>
2023-12-14 12:46:09 +0900

    siteのベースURLを正しく修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue(), tailwind()],
-  site: 'https://hiroshijp.github.io',
+  site: 'https://hiroshijp.github.io/blog',
 });
